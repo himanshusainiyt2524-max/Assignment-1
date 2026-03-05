8df9f00e02676d2af0dda91c8c9f9b5105169727
himanshusainiyt2524-max <himanshusainiyt2524@gmail.com>
2026-03-05 22:17:49 +0530

    Update ASSIGNMENT 1 TASK 2.py

--- ASSIGNMENT 1 TASK 2.py
+++ ASSIGNMENT 1 TASK 2.py
@@ -1,5 +1,12 @@
+
+"""Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
 namef=input("enter your first name: ")
 namel=input("enter your last name: ")
-print("enter your first name:",namef)
-print("enter your last name:",namel)
+
 print("hello"",",namef,namel,"!","wellcom to python")
+
